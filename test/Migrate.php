506cbcdf5df61a5094376bbaff3b25310b24e5e0
Andy Theuninck <andy@gohanman.com>
2016-06-22 20:55:16 -0500

    Fix migrate test semicolon

--- test/Migrate.php
+++ test/Migrate.php
@@ -2,11 +2,14 @@
 
 class Migrate extends PHPUnit_Framework_TestCase
 {
-    $dbc = new COREPOS\common\SQLManager('localhost', 'PDO_MYSQL', 'test', 'root', '');
-    if (!class_exists('MockModel2')) {
-        include(__DIR__ . '/MockModel2.php');
+    function t()
+    {
+        $dbc = new COREPOS\common\SQLManager('localhost', 'PDO_MYSQL', 'test', 'root', '');
+        if (!class_exists('MockModel2')) {
+            include(__DIR__ . '/MockModel2.php');
+        }
+        $m = new MockModel2($dbc); 
+        var_dump($m->normalize('test'));
     }
-    $m = new MockModel2($dbc); 
-    var_dump($m->normalize('test'));
 }
 
